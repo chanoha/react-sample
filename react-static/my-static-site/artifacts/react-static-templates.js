dfb2740ddbd108fb34f08c81b279f88ce880d7fd
Takashi Fujimoto <takashi@TakashinoMacBook-Pro.local>
2020-01-15 22:36:23 +0900

    調整

---
+++ react-static/my-static-site/artifacts/react-static-templates.js
@@ -0,0 +1,14 @@
+
+
+  
+// Template Map
+export default {
+  '/Users/takashi/Documents/design/github/chanoha.github.io/react-static/my-static-site/src/pages/404.js': require('/Users/takashi/Documents/design/github/chanoha.github.io/react-static/my-static-site/src/pages/404.js').default,
+'/Users/takashi/Documents/design/github/chanoha.github.io/react-static/my-static-site/src/pages/about.js': require('/Users/takashi/Documents/design/github/chanoha.github.io/react-static/my-static-site/src/pages/about.js').default,
+'/Users/takashi/Documents/design/github/chanoha.github.io/react-static/my-static-site/src/pages/blog.js': require('/Users/takashi/Documents/design/github/chanoha.github.io/react-static/my-static-site/src/pages/blog.js').default,
+'/Users/takashi/Documents/design/github/chanoha.github.io/react-static/my-static-site/src/pages/index.js': require('/Users/takashi/Documents/design/github/chanoha.github.io/react-static/my-static-site/src/pages/index.js').default,
+'/Users/takashi/Documents/design/github/chanoha.github.io/react-static/my-static-site/src/containers/Post': require('/Users/takashi/Documents/design/github/chanoha.github.io/react-static/my-static-site/src/containers/Post').default
+}
+
+export const notFoundTemplate = '/Users/takashi/Documents/design/github/chanoha.github.io/react-static/my-static-site/src/pages/404.js'
+

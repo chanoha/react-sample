03b13b8d08972c774026e94891779e72cac5c3f1
Takashi Fujimoto <takashi@TakashinoMacBook-Pro.local>
2020-01-18 21:17:45 +0900

    stop

--- react-static/my-static-site/static.config.js
+++ react-static/my-static-site/static.config.js
@@ -10,6 +10,16 @@ export default {
     return [
       {
         path: '/blog',
+        paths: {
+          root: process.cwd(), // プロジェクトのルート
+          src: 'src', //ソースフォルダ。index.jsをエントリーポイントに含む必要あり。
+          temp: 'tmp', //ビルドで発生する公開しないファイルの置き場。
+          dist: 'dist', //本番用出力フォルダ
+          devDist: 'tmp/dev-server', //The development scratch directory.
+          public: 'public', //The public directory (files copied to dist during build)
+          assets: 'dist', //The output directory for bundled JS and CSS
+          buildArtifacts: 'artifacts', //The output directory for generated (internal) resources
+        },
         getData: () => ({
           posts,
         }),

86fc02d39d77eff1f2cdbd198b22cc73c0233ae9
Takashi Fujimoto <takashi@TakashinoMacBook-Pro.local>
2020-02-11 16:02:55 +0900

    add styled-components

---
+++ firebase-app/src/app/pages/TitleStyle.tsx
@@ -0,0 +1,9 @@
+// import React from 'react';
+import styled from 'styled-components';
+
+export const H1 = styled.h1`
+    font-size: 50px;
+    text-align: center;
+`;
+
+export default H1;
--- firebase-app/src/app/pages/index.tsx
+++ firebase-app/src/app/pages/index.tsx
@@ -1,7 +1,9 @@
 import App from '../components/App';
+import H1 from './TitleStyle'
 
 export default () => (
   <App>
     <p>Top Page</p>
+    <H1>style text</H1>
   </App>
 );

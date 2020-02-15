5be25dc7bb94b57303af845708a44e559e76e031
Takashi Fujimoto <takashi@TakashinoMacBook-Pro.local>
2020-02-15 20:24:17 +0900

    add normalize.css

--- firebase-app/src/app/pages/index.tsx
+++ firebase-app/src/app/pages/index.tsx
@@ -1,8 +1,11 @@
 import App from '../components/App';
+import React from 'react';
+import { Normalize } from 'styled-normalize';
 import { H1 } from './TitleStyle';
 
 export default () => (
   <App>
+    <Normalize />
     <p>Top Page</p>
     <H1>style text</H1>
   </App>

464592d51a0742189f713129e5166f613a52b8db
Takashi Fujimoto <takashi@TakashinoMacBook-Pro.local>
2020-02-16 17:02:42 +0900

    change style

---
+++ firebase-app/src/app/components/organism/MainVisual/MainVisual.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const MainVisual = () => (
+    <MainVisualStyle>
+        <h1>MainVisual</h1>
+    </MainVisualStyle>
+);
+
+const MainVisualStyle = styled.div`
+    background: #E3E3E3;
+    width: 100%;
+    height: 300px;
+    color: #fff;
+`;
+
+export default MainVisual;
--- firebase-app/src/app/pages/index.tsx
+++ firebase-app/src/app/pages/index.tsx
@@ -1,12 +1,19 @@
 import App from '../components/App';
-import React from 'react';
+import MainVisual from '../components/organism/MainVisual/MainVisual';
 import { Normalize } from 'styled-normalize';
-import { H1 } from './TitleStyle';
+import styled from 'styled-components';
 
 export default () => (
   <App>
     <Normalize />
+    <MainVisual />
     <p>Top Page</p>
     <H1>style text</H1>
   </App>
 );
+
+const H1 = styled.h1`
+    font-size: 50px;
+    color: blue;
+    text-align: center;
+`;

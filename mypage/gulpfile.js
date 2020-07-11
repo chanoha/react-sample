688bf34e559ff4c1bc35a55532e0dc1511ef2ca2
Takashi Fujimoto <takashi@TakashinoMacBook-Pro.local>
2020-07-11 23:12:56 +0900

    add watch

--- mypage/gulpfile.js
+++ mypage/gulpfile.js
@@ -5,17 +5,20 @@ const sass = require("gulp-sass");
 
 // style.scssをタスクを作成する
 gulp.task("default", function() {
-  // style.scssファイルを取得
-  return (
-    gulp
-    .src("css/style.scss")
-    // Sassのコンパイルを実行
-    .pipe(
-        sass({
-            outputStyle: "expanded"
-        })
-    )
-    // cssフォルダー以下に保存
-    .pipe(gulp.dest("css"))
-  );
+    // scssファイルの監視
+    return gulp.watch("css/style.scss", function(){
+        // style.scssファイルを取得
+        return (
+            gulp
+            .src("css/style.scss")
+            // Sassのコンパイルを実行
+            .pipe(
+                sass({
+                    outputStyle: "expanded"
+                })
+            )
+            // cssフォルダー以下に保存
+            .pipe(gulp.dest("css"))
+        );
+    });
 });
